7517527a61956a49bc3fc97153bdf9513f74d225
Rahul Gupta <assitch96@gmail.com>
2016-07-02 22:57:11 +0530

    Improved ui

--- jscript.js
+++ jscript.js
@@ -4,9 +4,8 @@ var main = function () {
 	var openWeatherMapKey = "bfd3b440167863075755d5567ff8b227";
 	var weatherIcons = $.getJSON("https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json");
 	var latitude, longitude, city;
-	var C = [1, 1, 1, 1, 1];
-	var F = [0, 0, 0, 0, 0];
-	var temperatureK = [], temperatureC = [], temperatureF = [], weatherData = [], code = [];
+	var C = 1, F = 0;
+	var temperatureK = [], temperatureC = [], temperatureF = [], weatherData = [], date = [], code = [];
 	// getting the value of the latitude and longitude of the user from ip-api.com
 	$.getJSON(api_ip, function(data) {
 		$.each(data, function(key, value){ 			
@@ -21,12 +20,14 @@ var main = function () {
 			temperatureK[0] = data.list[0].main.temp;
 			weatherData[0] = data.list[0].weather[0].main;
 			code[0] = (data.list[0].weather[0].id).toString();
+			date[0] = (data.list[0].dt_txt).substring(9, 10) + "-" + (data.list[0].dt_txt).substring(5, 7);
 			// storing the temperature, weather, and respective code for icon data of 5 consecutive days in an array temperatureK, weatherData, and code
 			while(index < (data.list).length) {
 				if((data.list[index].dt_txt).substring(11, 19) == "00:00:00") {
 					temperatureK[i] = data.list[index].main.temp;
 					weatherData[i] = data.list[index].weather[0].main;
 					code[i] = (data.list[index].weather[0].id).toString();
+					date[i] = (data.list[index].dt_txt).substring(9, 10) + "-" + (data.list[index].dt_txt).substring(5, 7);
 					i ++;
 				}
 				index ++;
@@ -39,13 +40,32 @@ var main = function () {
 			// printing out the data
 			$('._city').html(city);
 			for(i = 0; i < 5; i ++) {
-				$('._weather' + (i + 1)).html(weatherData[i]);
 				$('._temp' + (i + 1)).html((temperatureC[i]).toFixed(0));
-				$('._icon' + (i + 1)).addClass("wi-owm-" + code[i]);
+				$('._date' + (i + 1)).html(date[i]);
+				$('._icon' + (i + 1)).addClass("wi-owm-" + code[i]).css({'textShadow': '2px 5px 7px #242424'});
+				$('._weather' + (i + 1)).html(weatherData[i]);
+			}
+		});
+		$('._unit').on('click', function(){
+			if(C) {
+				$(this).removeClass('wi-celsius');
+				$(this).addClass('wi-fahrenheit');
+				for(i = 0; i < 5; i ++) {
+					$('._temp' + (i + 1)).html(temperatureF[i].toFixed(0));
+				}
+				C = 0; F = 1;
+			}
+			else {
+				$(this).removeClass('wi-fahrenheit');
+				$(this).addClass('wi-celsius');
+				for(i = 0; i < 5; i ++) {
+					$('._temp' + (i + 1)).html(temperatureC[i].toFixed(0));	
+				}
+				F = 0; C = 1;
 			}
 		});
 
-		$('._unit1').on('click', function(){
+		/*$('._unit1').on('click', function(){
 			if(C[0]) {
 				$(this).removeClass('wi-celsius');
 				$(this).addClass('wi-fahrenheit');
@@ -114,7 +134,7 @@ var main = function () {
 				$('._temp5').html(temperatureC[4].toFixed(0));
 				F[4] = 0; C[4] = 1;	
 			}
-		});
+		});*/
 	});
 };
 
