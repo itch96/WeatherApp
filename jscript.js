968f644c225249973c09c51677d72a6f91b0ba91
Rahul Gupta <assitch96@gmail.com>
2016-07-04 10:49:23 +0530

    Removed my open weather map key.

--- jscript.js
+++ jscript.js
@@ -2,7 +2,7 @@ var main = function () {
 	
 	var api_ip = "http://ip-api.com/json/";
 	var weatherIcons = $.getJSON("https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json");
-	var openWeatherMapKey = "bfd3b440167863075755d5567ff8b227";
+	var openWeatherMapKey = ""; // I guess I'm not supposed to share the key.
 	var latitude, longitude, city;
 	var temperatureK = [], temperatureC = [], temperatureF = [], weatherData = [], date = [], code = [], minTempK = [], maxTempK = [], minTempC = [], maxTempC = [], minTempF = [], maxTempF = [], humidity = [], windSpeed = [];
 	// getting the value of the latitude and longitude of the user from ip-api.com
